5ba2da84e0fa841e3bf02784cce913ab1133a7d2
khushishetty007 <khushishetty256@gmail.com>
2026-05-18 23:28:43 +0530

    day 2/150

---
+++ 2.py
@@ -0,0 +1,19 @@
+#concatination
+first_name="Khushi"
+second_name="Shetty"
+full_name=first_name+" "+second_name
+print(full_name)
+
+#repetition
+message="Warning! "
+print(message*20)
+
+print(message.upper())    #upper
+print(message.lower())    #lower
+print(message.strip()*2)  #strip
+print(message.replace("Warning!","Error"))   #replace
+print(len(message))#length
+print(message[1])
+print(message[2:6])
+print(message[:2])
+print(message[::3])
