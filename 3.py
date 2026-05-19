50ac86cd0458d991f2615494302de63dec3c55d7
khushishetty007 <khushishetty256@gmail.com>
2026-05-19 22:12:20 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,30 @@
+#OPERATORS
+#1.common assignment operator
+a=100
+a+=10
+print(a)
+#example 2
+a=10
+a*=20
+print(a)
+print("------")
+#2.Comparison Operator
+a=10
+b=20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print("------")
+
+#Logical Operators:AND,OR,NOT
+#AND
+print(2>1 and 2<1)
+print(2>1 or 2<1)
+print(not(2>1))
+print(not(2<1))
+print("------")
+
+#Membership Operator
+name_1="khushi"
+print("h" in name_1)
